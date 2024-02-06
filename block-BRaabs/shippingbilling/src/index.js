338af44acbed5f2ee43b634a7bcfe33420968e4d
KASHISH <kashu80525@gmail.com>
2024-02-06 20:08:26 +0530

    adding shipping and billing address

---
+++ block-BRaabs/shippingbilling/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+//import Controlled from "./Controlled";
+// import Uncontrolled from "./Uncontrolled";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
